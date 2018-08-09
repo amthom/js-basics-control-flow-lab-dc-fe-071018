9dc48fcef436fa55f6837a101da3fd99ff7b79c3
Amanda Thomas <amandamariathomas@gmail.com>
2018-08-09 23:58:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 function scuberGreetingForFeet (distance) {
-  console.log ("== scuberGreetingForFeet ==");
+  console.log("== scuberGreetingForFeet ==");
   if (distance <= 400) {
     let response = "This one is on me"
   }
